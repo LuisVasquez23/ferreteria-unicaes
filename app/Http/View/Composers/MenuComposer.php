044406edeeed3e75d914bec8d2c587eadc2309ec
Luis Vasquez <vluis5457@gmail.com>
2023-09-02 21:31:11 -0600

    Agregando menu list

--- app/Http/View/Composers/MenuComposer.php
+++ app/Http/View/Composers/MenuComposer.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\View\Composers;
 
+use App\Models\DetalleRole;
 use Illuminate\View\View;
 use App\Models\MenuOption;
+use App\Models\Usuario;
 use Illuminate\Support\Facades\Auth;
 
 class MenuComposer
@@ -14,8 +16,18 @@ class MenuComposer
         $user = Auth::user();
 
         if ($user) {
-            // Obtén las opciones de menú basadas en el rol del usuario
-            $menuOptions = MenuOption::whereNull('parent_id')->get();
+            // Obtén el ID del usuario
+            $userId = Auth::user()->usuario_id;
+
+            // Obtén todos los roles del usuario
+            $todosRoles = DetalleRole::where('usuario_id', $userId)->get();
+
+            // Crea un arreglo de IDs de roles
+            $rolesIds = $todosRoles->pluck('role_id')->toArray();
+
+            // Obtén las opciones de menú basadas en los roles del usuario
+            $menuOptions = MenuOption::whereIn('role_id', $rolesIds)
+                ->get();
 
             // Comparte las opciones de menú con la vista
             $view->with('menuOptions', $menuOptions);
