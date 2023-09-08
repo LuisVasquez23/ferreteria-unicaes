4ce4821873a72d6e9e3fe9f6036310ef5530dc03
Martin <fjose6586@gmail.com>
2023-09-07 22:06:45 -0600

    vistas

--- routes/web.php
+++ routes/web.php
@@ -43,4 +43,6 @@ require __DIR__ . '/auth.php';
 require __DIR__ . '/AdminMenu/menu.php';
 require __DIR__ . '/AdminRoles/roles.php';
 require __DIR__ . '/AdminCategorias/categorias.php';
+
+
 require __DIR__ . '/AdminEmpleados/empleados.php';
