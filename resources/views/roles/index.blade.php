043a6ca435d0b4b24f9377a84613135c46c1712a
Luis Vasquez <vluis5457@gmail.com>
2023-09-17 20:38:07 -0600

    Cambiando orden de migraciones

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -5,7 +5,9 @@
     <div class="card mt-3">
         <h5 class="card-header">Administracion del roles</h5>
         <div class="card-body">
-            <a href="{{ route('menu.create') }}" class="btn btn-success mb-3">Agregar</a>
+            <a href="{{ route('roles.create') }}" class="btn btn-success mb-3">
+                <i class="fas fa-plus"></i> Agregar role
+            </a>
             <div class="table-responsive">
                 <table class="table text-nowrap mb-0 align-middle table-striped table-bordered" id="miTabla">
                     <thead class="text-dark fs-4">
@@ -22,7 +24,49 @@
                         </tr>
                     </thead>
                     <tbody>
+                        @foreach ($roles as $role)
+                            <tr>
+                                <td>
+                                    <h6 class="fw-semibold mb-0">{{ $role->role }}</h6>
+                                </td>
+                                <td>
+                                    <h6 class="fw-semibold mb-0">{{ $role->descripcion }}</h6>
+                                </td>
+                                <td class="d-flex gap-1 justify-content-center">
+                                    @if ($filtro !== 'bloqueados')
+                                        <a href="{{ route('roles.edit', $role->role_id) }}" class="btn btn-primary">
+                                            <i class="ti ti-pencil"></i>
+                                        </a>
+                                    @endif
 
+                                    @if ($filtro !== 'bloqueados')
+                                        <form action="{{ route('roles.destroy', $role->role_id) }}" method="POST"
+                                            id="block-form-{{ $role->role_id }}">
+                                            @csrf
+                                            @method('DELETE')
+                                            <input type="hidden" name="action" value="update">
+                                            <button type="button" class="btn btn-danger"
+                                                onclick="confirmBlock({{ $role->role_id }})">
+                                                <i class="fa-solid fa-lock"></i>
+                                            </button>
+                                        </form>
+                                    @endif
+
+
+                                    @if ($filtro === 'bloqueados')
+                                        <form action="{{ route('cliente.unblock', $role->role_id) }}" method="POST"
+                                            id="unblock-form-{{ $role->role_id }}">
+                                            @csrf
+                                            @method('PUT')
+                                            <button type="button" class="btn btn-warning"
+                                                onclick="confirmUnblock({{ $role->role_id }})">
+                                                <i class="fa-solid fa-unlock"></i>
+                                            </button>
+                                        </form>
+                                    @endif
+                                </td>
+                            </tr>
+                        @endforeach
                     </tbody>
                 </table>
             </div>
