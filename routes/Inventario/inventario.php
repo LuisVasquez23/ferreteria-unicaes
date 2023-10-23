a65d4d2bfbddd47b738ed487158beda5f6945d8a
Luis Vasquez <vluis5457@gmail.com>
2023-10-23 16:43:15 -0600

    Finaliado

--- routes/Inventario/inventario.php
+++ routes/Inventario/inventario.php
@@ -5,17 +5,8 @@ use App\Http\Controllers\InventarioController;
 use Illuminate\Support\Facades\Route;
 
 
-<<<<<<< HEAD
-Route::middleware(['auth', 'verified', 'checkRole:Admin,MegaAdmin,Empleado'])->group(function () {
-    Route::get('/inventario', [InventarioController::class, 'index'])->name('inventario.index');
 
-
-=======
 Route::controller(InventarioController::class)->group(function () {
-
     Route::get('/inventario', 'index')->middleware(['auth', 'verified', 'checkRole:Admin,MegaAdmin,Empleado'])->name('inventario.index');
-    Route::get('/inventario', 'validarCantidadProductos')->middleware(['auth', 'verified', 'checkRole:Admin,MegaAdmin,Empleado'])->name('inventario.productos_cantidad');
->>>>>>> 6d5e5a1b0f160ae221680ebd987a9dd374d11c05
+    Route::get('/inventario/validarCantidadProductos', 'validarCantidadProductos')->middleware(['auth', 'verified', 'checkRole:Admin,MegaAdmin,Empleado'])->name('inventario.productos_cantidad');
 });
-
-
